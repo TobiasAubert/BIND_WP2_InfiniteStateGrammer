0703dd1f5bc342f80e25ad918db750344b65f96d
tob1666 <tobias@c-aubert.com>
2025-09-06 11:09:46 +0200

    added function to load the sequences from the txt automatically

---
+++ src/load_sequences.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+def load_sequences(folder: Path):
+    sequences = {}
+
+    for file in folder.glob("*.txt"):
+        with open(file, encoding="utf-8") as f:
+            seq = [int(line.strip()) for line in f if line.strip()]
+        sequences[file.stem] = seq # use filename without extension as key
+
+    return sequences
